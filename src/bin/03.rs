9943882761809657f2c4974cc1addb487abfed95
Rob Rodi <1569969+robrodi@users.noreply.github.com>
2025-12-17 15:27:09 -0800

    part 2 done

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -4,12 +4,12 @@ pub fn part_one(input: &str) -> Option<u64> {
     let result = input
         .lines()
         .filter(|line| !line.is_empty())
-        .map(best_joltage)
+        .map(best_joltage_2)
         .sum();
         
     Some(result)
 }
-pub fn best_joltage(line: &str) -> u64 {
+pub fn best_joltage_2(line: &str) -> u64 {
     if line.len() == 0 { return 0; }
 
     let max = line [0..line.len()-1].chars().max().unwrap();
@@ -21,43 +21,55 @@ pub fn best_joltage(line: &str) -> u64 {
 
     10 * (max.to_digit(10).unwrap() as u64) + (next_max.to_digit(10).unwrap() as u64)
 }
+pub fn best_joltage_12(line: &str) -> u64 {
+    if line.len() == 0 { return 0; }
 
-// unused.
-pub fn all_number_pairs(line: &str) -> Vec<u64> {
-    let chars: Vec<char> = line.chars().collect();
-    let len = chars.len();
-    let mut result = Vec::new();
+    let mut max = Vec::new();
+    let mut search_start = 0;
 
-    for i in 0..(len - 1) {
-        let num = if i < (len - 2) { 
-                            format!("{}{}", chars[i], chars[i+1]).parse::<u64>().unwrap() 
-                        } else { 
-                            chars[i].to_string().parse::<u64>().unwrap() 
-                        }; // chars[i+1];
-        println!("Char Pair at index {} is {:2}", i, num);
-        result.push(num);
+    for i in 0..12 {
+        let search_legth = 12 - i;
+        let search_end = line.len() - search_legth + 1;
+        if search_end <= search_start { break; }
+
+        let segment = &line[search_start..search_end];
+        let segment_max = segment.chars().max().unwrap();
+        max.push(segment_max);
+
+        search_start += segment.find(segment_max).unwrap() + 1;
     }
-    result
+    
+    let number: String = max.into_iter().collect();
+    number.parse::<u64>().unwrap()
 }
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let result = input
+        .lines()
+        .filter(|line| !line.is_empty())
+        .map(best_joltage_12)
+        .sum();
+        
+    Some(result)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test_pairs(){
-        let result = all_number_pairs("1234567890");
-        assert_eq!(result.len(), 9);
-        assert_eq!(result, vec![12,23,34,45,56,67,78,89,9]);
+    fn test_joltage_2(){
+        assert_eq!(best_joltage_2("987654321111111"), 98);
+        assert_eq!(best_joltage_2("811111111111119"), 89);
+        assert_eq!(best_joltage_2("234234234234278"), 78);
+        assert_eq!(best_joltage_2("818181911112111"), 92);
     }
+
     #[test]
-    fn test_joltage(){
-        assert_eq!(best_joltage("987654321111111"), 98);
-        assert_eq!(best_joltage("811111111111119"), 89);
-        assert_eq!(best_joltage("234234234234278"), 78);
-        assert_eq!(best_joltage("818181911112111"), 92);
+    fn test_joltage_12(){
+        assert_eq!(best_joltage_12("987654321111111"), 987654321111);
+        assert_eq!(best_joltage_12("811111111111119"), 811111111119);
+        assert_eq!(best_joltage_12("234234234234278"), 434234234278);
+        assert_eq!(best_joltage_12("818181911112111"), 888911112111);
     }
 
     #[test]
@@ -69,6 +81,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3121910778619));
     }
 }
