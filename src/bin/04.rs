86485ecadf7dd8229297aad5bbac5b937dcdfc6d
Rob Rodi <1569969+robrodi@users.noreply.github.com>
2025-12-17 21:31:23 -0800

    part 2 done

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,32 +1,40 @@
 advent_of_code::solution!(4);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let lines= lines(input);
+    let mut map = input;
+    let (accessible_rolls, new_map) = move_rolls(&map);
+    println!("{}", new_map);
+    Some(accessible_rolls)
+}
 
+fn move_rolls(input: &str) -> (u64, String) {
+let lines= lines(input);
     let _height = lines.len();
     let _width = lines[0].len();
+    let mut new_map = String::new();
 
     let mut accessible_rolls = 0;
     for h in 0..lines.len() {
         for w in 0..lines[0].len() {
             if !is_roll(h, w, &lines) { 
-                print!(".");
+                new_map.push('.');
                 continue; 
             }
             let neighbors = num_neighbors(h, w, &lines);
             if neighbors < 4 { 
                 accessible_rolls += 1; 
-                print!("x");
+                new_map.push('x');
             }
             else {
-                print!("@");
+                new_map.push('@');
             }
         }
-        println!();
-    }
-
-    Some(accessible_rolls)
+        new_map.push('\n');
+    }    
+    (accessible_rolls, new_map)
+    
 }
+
 fn lines(input: &str) -> Vec<&str> {
     input
         .lines()
@@ -59,8 +67,20 @@ fn num_neighbors(h: usize, w: usize, lines: &Vec<&str>) -> u32 {
                 .filter(|&y|y).count() as u32
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut map: String = input.to_string();
+    let mut net_rolls = 0;
+    loop{
+        let (accessible_rolls, new_map) = move_rolls(&map);
+        
+        if accessible_rolls == 0 { break; }
+        
+        net_rolls += accessible_rolls;
+        map = new_map.to_string();
+    }
+
+//    println!("{}", new_map);
+    Some(net_rolls)
 }
 
 #[cfg(test)]
@@ -93,6 +113,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(43));
     }
 }
